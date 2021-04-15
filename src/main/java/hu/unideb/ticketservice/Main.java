b80b739641257e9d05773c961b711c85a4070477
raczandras <raczandras0204@gmail.com>
2021-04-15 19:15:26 +0200

    Created the main class

--- src/main/java/hu/unideb/ticketservice/Main.java
+++ src/main/java/hu/unideb/ticketservice/Main.java
@@ -1,4 +1,17 @@
 package hu.unideb.ticketservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(Main.class, args);
+
+    }
+
 }
+
